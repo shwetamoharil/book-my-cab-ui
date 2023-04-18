33e7cd55e4f3b84f1cefe7e97082db539af64dd7
shwetamoharil <shwetavmmoharil@gmail.com>
2023-04-18 11:36:22 +0530

    feat: added translation package

---
+++ src/i18n.js
@@ -0,0 +1,22 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import translationEn from "./locales/en/translation.json";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: translationEn,
+      },
+    },
+  });
+
+export default i18n;
