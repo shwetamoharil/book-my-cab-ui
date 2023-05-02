64f68326efb122da8936386fbdb6d718f58ab32a
Nitin Srivastava <nitin.srivastava@gslab.com>
2023-05-02 11:36:09 +0530

    chore: firebase config and linting

---
+++ src/environment/environment.js
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  baseUrl: "https://bmc-book-my-cab-default-rtdb.firebaseio.com/",
+  firebaseConfig: {
+    apiKey: "AIzaSyBCwQ7moLHbD3FFU9qZte7BQawbnn5jBTs",
+    authDomain: "bmc-book-my-cab.firebaseapp.com",
+    projectId: "bmc-book-my-cab",
+    storageBucket: "bmc-book-my-cab.appspot.com",
+    messagingSenderId: "8592100887",
+    appId: "1:8592100887:web:cc22be7514e27c9c94d652",
+  },
+};
